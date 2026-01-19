5462d7da686e0d6803cd80863bdf3cc699268be6
Sergio Lucero <sergiolucero73@gmail.com>
2026-01-19 20:30:33 -0300

    feat: implementacion de get_users en snr_test_pkg

---
+++ src/database/desa_schema/package_bodies/snr_test_pkg.sql
@@ -0,0 +1,47 @@
+create or replace package body desa_schema.snr_test_pkg as
+
+    function get_users return snr_users%rowtype as
+        cursor cr_data is
+        select
+            *
+        from
+            snr_users;
+    --
+        v_users cr_data%rowtype;
+    begin
+        open cr_data;
+        fetch cr_data into v_users;
+        close cr_data;
+        return v_users;
+    end get_users;
+
+    function get_user_by_id (
+        p_id snr_users.id%type
+    ) return snr_users%rowtype as
+    begin
+    -- TAREA: Se necesita implantación para function SNR_TEST_PKG.get_user_by_id
+        return null;
+    end get_user_by_id;
+
+    procedure create_user (
+        p_user snr_users%rowtype
+    ) as
+    begin
+    -- TAREA: Se necesita implantación para procedure SNR_TEST_PKG.create_user
+        null;
+    end create_user;
+
+    procedure update_user (
+        p_id   snr_users.id%type,
+        p_user snr_users%rowtype
+    ) as
+    begin
+    -- TAREA: Se necesita implantación para procedure SNR_TEST_PKG.update_user
+        null;
+    end update_user;
+
+end snr_test_pkg;
+/
+
+
+-- sqlcl_snapshot {"hash":"312ac7c66e510dce681fcda92766645f4632f505","type":"PACKAGE_BODY","name":"SNR_TEST_PKG","schemaName":"DESA_SCHEMA","sxml":""}
