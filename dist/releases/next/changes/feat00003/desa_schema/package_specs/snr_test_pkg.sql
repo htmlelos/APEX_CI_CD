53fd7f8ecae551bcac7e610cc61fa38060a6e919
Sergio Lucero <sergiolucero73@gmail.com>
2026-02-10 19:58:38 -0300

    fix(db): remove hardcoded schema references for portable deployment

--- dist/releases/next/changes/feat00003/desa_schema/package_specs/snr_test_pkg.sql
+++ dist/releases/next/changes/feat00003/desa_schema/package_specs/snr_test_pkg.sql
@@ -2,7 +2,7 @@
 -- changeset DESA_SCHEMA:1768865290006 stripComments:false  logicalFilePath:feat00003\desa_schema\package_specs\snr_test_pkg.sql
 -- sqlcl_snapshot src/database/desa_schema/package_specs/snr_test_pkg.sql:null:d974372cd4f0ed43c6581de2d3b5efc4607eb976:create
 
-create or replace package desa_schema.snr_test_pkg as 
+create or replace package snr_test_pkg as 
   /* TODO enter package declarations (types, exceptions, methods etc) here */
 
     function get_users return snr_users%rowtype;
